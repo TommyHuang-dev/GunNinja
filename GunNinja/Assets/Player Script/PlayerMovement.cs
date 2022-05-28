92136f65e12f02291da0a8cb5adeb13695a96ca4
Tommy Huang <tommyhuang999@gmail.com>
2022-05-28 03:24:49 -0400

    add test bridge

--- GunNinja/Assets/Player Script/PlayerMovement.cs
+++ GunNinja/Assets/Player Script/PlayerMovement.cs
@@ -22,10 +22,10 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         cam = Camera.main;
-        accel = 1.5f;
-        maxSpeed = 8;  // max horizontal speed
+        accel = 1.2f;
+        maxSpeed = 10;  // max horizontal speed
         distToGround = GetComponent<Collider>().bounds.extents.y;
-        jumpStrength = 6;
+        jumpStrength = 8;
         doublejump = true;
     }
 
@@ -54,8 +54,8 @@ public class PlayerMovement : MonoBehaviour
             
         if (isGrounded)
         {
-            rb.drag = 5;
-            accel = 1.5f;
+            rb.drag = 8;
+            accel = 1.2f;
             doublejump = true;
             if (Input.GetKeyDown("space"))
             {
@@ -67,7 +67,7 @@ public class PlayerMovement : MonoBehaviour
                 // stop character from flying going over small ramps
                 Ray ray = new Ray(rb.transform.position + new Vector3(0, 0.1f, 0), -Vector3.up);
                 RaycastHit rayHit;
-                if (Physics.Raycast(ray, out rayHit, distToGround + 0.2f)) {
+                if (Physics.Raycast(ray, out rayHit, distToGround + 0.25f)) {
                     rb.MovePosition(rb.transform.position + new Vector3(0, - rayHit.distance + distToGround + 0.1f, 0));
                 }
             }
@@ -75,13 +75,13 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             rb.drag = 0.2f;
-            accel = 0.1f;
+            accel = 0.05f;
             if (doublejump && Input.GetKeyDown("space"))
             {
                 doublejump = false;
                 if (rb.velocity.y < 0) { rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z); }
-                rb.velocity += new Vector3(0, jumpStrength, 0);
-                accel = 1.5f;
+                rb.velocity += new Vector3(0, jumpStrength * 0.8f, 0);
+                accel = 1.2f;
             }
         }
 
@@ -90,19 +90,19 @@ public class PlayerMovement : MonoBehaviour
             var vect = new Vector3(0, 0, 0);
             if (Input.GetKey("w"))
             {
-                vect = new Vector3(rb.transform.forward.x, 0, rb.transform.forward.z).normalized;
+                vect = (vect + new Vector3(rb.transform.forward.x, 0, rb.transform.forward.z).normalized).normalized;
             }
             if (Input.GetKey("s"))
             {
-                vect = new Vector3(- rb.transform.forward.x, 0, - rb.transform.forward.z).normalized;
+                vect = (vect +new Vector3(- rb.transform.forward.x, 0, - rb.transform.forward.z).normalized).normalized;
             }
             if (Input.GetKey("a"))
             {
-                vect = new Vector3(-rb.transform.right.x, 0, -rb.transform.right.z).normalized;
+                vect = (vect + new Vector3(-rb.transform.right.x, 0, -rb.transform.right.z).normalized).normalized;
             }
             if (Input.GetKey("d"))
             {
-                vect = new Vector3(rb.transform.right.x, 0, rb.transform.right.z).normalized;
+                vect = (vect + new Vector3(rb.transform.right.x, 0, rb.transform.right.z).normalized).normalized;
             }
             rb.velocity += vect * accel;
 
